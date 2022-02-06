b45f4b53e3bf06f6eee46c46a30ea518511cf348
samuelfuchs <fuchsgerson@gmail.com>
2022-02-06 02:15:28 -0300

    almost finished

--- client/src/Components/Calendar.jsx
+++ client/src/Components/Calendar.jsx
@@ -2,16 +2,36 @@ import React, { useRef, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import AddEventModal from "./AddEventModal";
+import axios from "axios";
+import moment from "moment";
 
 export default function () {
   const [modalOpen, setModalOpen] = useState(false);
-
+  const [events, setEvents] = useState([]);
   const calendarRef = useRef(null);
 
   const onEventAdded = (event) => {
     let calendarApi = calendarRef.current.getApi();
-    calendarApi.addEvent(event);
+    calendarApi.addEvent({
+      start: moment(event.start).toDate(),
+      end: moment(event.end).toDate(),
+      title: event.title,
+    });
   };
+
+  async function handleEventAdd(data) {
+    await axios.post("/api/calendar/create-event", data.event);
+  }
+
+  async function handleDatesSet(data) {
+    const response = await axios.get(
+      "/api/calendar/get-events?start=" +
+        moment(data.start).toISOString() +
+        "&end=" +
+        moment(data.end).toISOString()
+    );
+    setEvents(response.data);
+  }
 
   return (
     <section>
@@ -19,8 +39,11 @@ export default function () {
       <div style={{ position: "relative", zIndex: 0 }}>
         <FullCalendar
           ref={calendarRef}
+          events={events}
           plugins={[dayGridPlugin]}
           initialView="dayGridMonth"
+          eventAdd={(event) => handleEventAdd(event)}
+          datesSet={(date) => handleDatesSet(date)}
         />
       </div>
 
