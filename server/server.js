9cac89e2a602e5b7cb24510a84142b545a4deddd
samuelfuchs <fuchsgerson@gmail.com>
2022-02-05 23:57:31 -0300

    server setup update

--- server/server.js
+++ server/server.js
@@ -6,3 +6,14 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  () => console.log("Connected to MongoDB")
+);
+
+app.listen(5000, () => console.log('Server started');)
