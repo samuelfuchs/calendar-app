2b46e7a88bf7ac52dce3525f567a9d4f3ea2d25c
samuelfuchs <fuchsgerson@gmail.com>
2022-02-05 23:52:35 -0300

    server setup

---
+++ server/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
